121c99fdca6fffcd4507bf3e57f6b031bed648f2
Sebastian Hancock <sebastian.hancock@uky.edu>
2016-11-16 22:40:09 -0500

    random tests

---
+++ script/test-script.js
@@ -0,0 +1,16 @@
+/* var xhr = new XMLHttpRequest();
+
+xhr.open('GET', "//ipinfo.io/json", true);
+xhr.send();
+
+xhr.addEventListener("readystatechange", processRequest, false);
+
+xhr.onreadystatechange = processRequest;
+
+function processRequest(e){
+    if (xhr.readyState == 4 && xhr.status == 200){
+        var response = JSON.parse(xhr.responseText);
+        console.log(response);
+    }
+}
+*/
